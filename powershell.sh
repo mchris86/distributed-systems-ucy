97630f86c66d6c93720424204549b6314b7baeae
Michalis Christodoulou <michchristo@gmail.com>
2025-11-26 16:17:58 +0200

    add powershell script for quiz

---
+++ powershell.sh
@@ -0,0 +1,16 @@
+# Create venv
+python -m venv .venv
+.venv\Scripts\Activate.ps1
+
+pip install pyzmq
+
+# Start client
+Start-Process powershell -ArgumentList "python client.py"
+
+# Start coordinator
+Start-Process powershell -ArgumentList "python coordinator.py"
+
+# Start participants
+Start-Process powershell -ArgumentList "python participant.py 5556"
+Start-Process powershell -ArgumentList "python participant.py 5557"
+Start-Process powershell -ArgumentList "python participant.py 5558"
